e71b71e4c32edb26964db2e39acc73abcb0dd5a6
DarkUwU <85587928+vaxnoo2@users.noreply.github.com>
2021-08-17 16:04:23 +0100

    Add files via upload

---
+++ soporte.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+print("Metodos para mandar a soporte")
+
+def validabool1(cadena):
+	if cadena.upper() == 'SR':
+		return True
+	else:
+		return False
+
+print("Metodos:")
+print("1) Metodo 1\n")
+print("2) Metodo 2\n")
+print("3) Como salir de soporte\n")
+print("4) Como restablecer codes\n")
+print("5) Metodo 3\n")
+	
+
+#dar entrada a la opcion
+op = input("selecciona una opcion, porfavor uwu: ")
+if op=="1":
+	print("Asunto: Deactivate My Account Number.")
+
+	print("Correo: Please Deactivate The My Account Number (número*) Immediately Because The Number Has Been Lost.")
+elif op=="2":
+	print("Desactivacion: Reportar el número 30 o 50 veces.")
+elif op=="3":
+	print("Salir de soporte")
+
+
+	print("Asunto: Mi nuevo número fue suspendido sin ningún motivo.")
+	print("Correo: Mi número es nuevo, y con él abrí recientemente una industria y en esta industria necesito urgentemente mi cuenta, no he roto ninguna regla de política de privacidad, por eso le pido al equipo de soporte de whatsapp que actúe lo antes posible para que pueda tener acceso a los materiales de mi industria (número)")
+elif op=="4":
+	print("restablecer codes")
+
+
+	print("Asunto: No puedo recibir el código de verificación de whatsapp.")
+	print("Correo: Mi número (número) está en soporte, alguien me pidió el código por error. Mi número que utilizo para trabajar Tengo una empresa de riesgo y necesito esta cuenta, por favor retire mi número. Reanuden mi SMS porfavor.")
+elif op=="5":
+	print("Metodo 3")
+
+	print("Tener un numero de reserva, crear un grupo para con el nombre CP, luego unen a los que quieren que vayan a soporte, luego reportan el grupo, en unos minutos whatsapp dara de baja todos los numeros")
+	print("psdt: Hasta dentro de 3 meses te devolveran el numero")
+else:
+	print("Eres bobo, kuaker malo?")
